bcee1a0477c679052c515f3dbaa16efd499128c7
benchang <ptst1110.ee10@nycu.edu.tw>
2024-01-13 23:29:35 +0800

    add api

---
+++ app/utils.py
@@ -0,0 +1,40 @@
+import sys
+import os
+import torch
+import torch.nn as nn 
+import torchvision.transforms as transforms 
+from PIL import Image
+import io
+import io
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from model import cat_classifier # import your model here
+
+
+
+def transform_image(image_bytes):
+    '''
+    transform image to tensor [1,3,224,224]
+    '''
+    transform = transforms.Compose([transforms.Resize(255),
+                                    transforms.CenterCrop(224),
+                                    transforms.ToTensor(),
+                                ])
+    image = Image.open(io.BytesIO(image_bytes)) #opening an image from a byte stream
+    
+    return transform(image).unsqueeze(0)
+
+
+# predict
+def get_prediction(image_tensor):
+    # load the model
+    PATH = '../checkpoint/model.pth' # path to your model checkpoint\
+    model = cat_classifier()
+    model.load_state_dict(torch.load(PATH)['model'],strict=False)
+    model.eval()
+    output = model(image_tensor)
+    pred = output.argmax(dim=1, keepdim=True)
+    print(pred)
+    # get the value of pred
+    predicted = pred.item()
+    
+    return predicted
