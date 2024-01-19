bb24e74363dddad184402cd869743fc73babfb2f
benchang <ptst1110.ee10@nycu.edu.tw>
2024-01-20 02:43:20 +0800

    add chinese support

--- predict_api/app/app.py
+++ predict_api/app/app.py
@@ -1,6 +1,15 @@
 from flask import Flask, request, jsonify
 from utils import transform_image, get_prediction, get_prediction_bi, transform_agriculture, get_prediction_leaf
+from flask import Response
+import json
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+classes = ['阿比西尼亞貓','孟加拉貓','伯曼貓','孟買貓','英國短毛貓','埃及貓','緬因貓','波斯貓','布偶貓','俄羅斯藍貓','暹羅貓',
+           '斯芬克斯貓','美國鬥牛犬','美國比特鬥牛犬','巴吉度獵犬','小獵犬','拳師狗','吉娃娃','英國可卡犬','英國塞特犬','德國短毛指示犬',
+           '庇里牛斯山犬','哈瓦那犬','狆','凱斯犬','蘭伯格犬','迷你杜賓犬','紐芬蘭犬','博美犬','巴哥犬','聖伯納犬',
+           '薩摩耶犬','蘇格蘭梗犬','柴犬','斯塔福郡鬥牛㹴','軟毛小麥梗','約克夏㹴']
+
 
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
@@ -25,7 +34,7 @@ def predict():
         tensor = transform_image(img_bytes) # image -> tensor
         if get_prediction_bi(tensor) == 1:
             prediction = get_prediction(tensor)
-            data = {'prediction': prediction}
+            data = {'prediction': classes[prediction]}
             return jsonify(data)
         else:
             return jsonify({'prediction': -1}) # -1 means not a cat and not a dog
@@ -47,7 +56,10 @@ def agriculture():
         prediction = get_prediction_leaf(tensor)
         class_names = ['healthy', 'rust1', 'rust2', 'rust3']
         data = {'prediction': class_names[prediction]}
-        return jsonify(data)
+        response = Response(response=json.dumps(data, ensure_ascii=False),
+                        status=200,
+                        mimetype="application/json; charset=utf-8")
+        return response
 
 @app.route('/', methods=['GET'])
 def index():
