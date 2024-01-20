0d07662d1b2595c1173324159950f578115acd8a
benchang <ptst1110.ee10@nycu.edu.tw>
2024-01-20 15:20:26 +0800

    add examples

--- predict_api/app/test.py
+++ predict_api/app/test.py
@@ -1,7 +1,7 @@
 import requests 
-
+import json
 # https://your-heroku-app-name.herokuapp.com/predict
 # http://localhost:5000/predict
-resp = requests.post("http://localhost:5000/predict", files={'file': open('test.jpg', 'rb')})
+resp = requests.post("http://localhost:5000/predict",data=json.dumps({"past_breeds": [0,12,2,33,10]}), headers={'Content-Type': 'application/json'})
 
-print(resp.json()['prediction'])
+print(resp.json())
