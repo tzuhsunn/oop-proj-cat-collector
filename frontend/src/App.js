de2d23ee769f7ec84373d57f5b99d4c185fdbb46
tzuhsun <hsungrass@gmail.com>
2024-01-21 08:35:50 +0000

    feat: frontend initial commit with image uploader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <ImageUploader />
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
--- frontend/src/components/ImageUploader.js
+++ frontend/src/components/ImageUploader.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Button, Typography, Input, Paper, Container } from '@mui/material';
 
 const ImageUploader = () => {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -49,20 +50,83 @@ const ImageUploader = () => {
     }
   };
 
-  return (
-    <div>
-      <h1>Coffee Image AI Detection</h1>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Ripe Coffee Detection</button>
+  const handleUploadArgriculture = async () => {
+    try {
+      if (!selectedFile) {
+        console.error('No file selected for upload.');
+        return;
+      }
 
-      {(imageUrl || predictionResult) && (
-        <div>
-          <h2>{predictionResult || 'Uploaded Image'}:</h2>
-          <img src={imageUrl} alt="Uploaded" style={{ maxWidth: '500px' }} />
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+
+      const response = await fetch('https://tzuhsun.online/cat/agriculture', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error('Upload failed');
+      }
+
+      const data = await response.json();
+      const result = await data.prediction;
+      console.log(data);
+
+      // Assuming the response contains the prediction result
+      setPredictionResult(result);
+    } catch (error) {
+      console.error('Error uploading image:', error);
+    }
+  };
+
+//   return (
+//     <div>
+//       <h1>Coffee Image AI Detection</h1>
+//       <input type="file" onChange={handleFileChange} />
+//       <button onClick={handleUpload}>Ripe Coffee Detection</button>
+//       <button onClick={handleUploadArgriculture}>Coffee Disease Detection</button>
+
+//       {(imageUrl || predictionResult) && (
+//         <div>
+//           <h2>{predictionResult || 'Uploaded Image'}</h2>
+//           <img src={imageUrl} alt="Uploaded" style={{ maxWidth: '500px', maxHeight: '500px' }} />
+//         </div>
+//       )}
+//     </div>
+//   );
+  
+// };
+
+return (
+  <Container maxWidth="sm">
+    <Paper elevation={3} style={{ padding: '20px', marginTop: '20px' }}>
+    <Typography variant="h4" gutterBottom>
+      Coffee Detection
+    </Typography>
+    <Input type="file" onChange={handleFileChange} style={{ marginBottom: '20px' }} />
+    <Button variant="contained" color="primary" onClick={handleUpload} style={{ width: '100px', marginRight: '10px' }}>
+      Ripeness
+    </Button>
+    <Button variant="contained" color="primary" onClick={handleUploadArgriculture} style={{ width: '100px' }}>
+      Disease
+    </Button>
+    {(imageUrl || predictionResult) && (
+        <div style={{ marginTop: '20px' }}>
+          <Typography variant="h5">
+            {predictionResult || 'Uploaded Image'}
+          </Typography>
+          <img
+            src={imageUrl}
+            alt="Uploaded"
+            style={{ maxWidth: '300px', maxHeight: '500px', marginTop: '10px' }}
+          />
         </div>
       )}
-    </div>
-  );
+    </Paper>
+  </Container>
+);
 };
+
 
 export default ImageUploader;
